af389857ac6e233ad298a5770ad79f744522d7b0
Youran Huang <huangmaidou608@outlook.com>
2026-06-13 18:26:24 +0800

    chore(works): add legend of vashain.

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,32 @@ export default defineConfig({
         {
           label: '作品',
           items: [
-            { label: '乌鸦的游记', link: '/works/crow-travelogue/' },
+            {
+              label: '乌鸦的游记',
+              items: [
+                { label: '简介', link: '/works/legend-of-vashain/' },
+                {
+                  label: '世界观与设定',
+                  items: [
+                    { label: '民族', link: '/works/legend-of-vashain/settings/nation/' },
+                    { label: '地理', link: '/works/legend-of-vashain/settings/geography/' },
+                    { label: '国家', link: '/works/legend-of-vashain/settings/countries/' },
+                    { label: '人物', link: '/works/legend-of-vashain/settings/characters/' },
+                    { label: '宗教', link: '/works/legend-of-vashain/settings/religion/' },
+                    { label: '其他', link: '/works/legend-of-vashain/settings/miscellany/' },
+                  ],
+                },
+                {
+                  label: '第一卷 日落岛',
+                  items: [
+                    { label: '第一章 铁匠之子', link: '/works/legend-of-vashain/volume-one-sunset-isle/chapter-1-blacksmiths-son/' },
+                    { label: '第二章 手套帮', link: '/works/legend-of-vashain/volume-one-sunset-isle/chapter-2-glove-gang/' },
+                    { label: '第三章 卡姆登镇的以撒人', link: '/works/legend-of-vashain/volume-one-sunset-isle/chapter-3-isaacites-of-camden/' },
+                    { label: '第四章 王河城', link: '/works/legend-of-vashain/volume-one-sunset-isle/chapter-4-riverking-city/' },
+                  ],
+                },
+              ],
+            },
           ],
         },
         {
